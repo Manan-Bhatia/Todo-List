04ea55d4398e4df2bbf2aec09b6eeff18ac3f36a
Manan Bhatia <mananbhatia02@gmail.com>
2024-02-28 13:24:00 +0530

    UI updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
 export default function Home() {
-    return <h1>Home Page</h1>;
+    const [todo, setTodo] = useState("");
+
+    const handleAddTodo = () => {
+        console.log(todo);
+        setTodo("");
+    };
+    return (
+        <div className="flex flex-col items-center gap-3">
+            <h1 className="text-2xl font-bold">Todo List</h1>
+            <span>
+                <input
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                    className="border-2 rounded-s-md p-2 focus:outline-none focus:border-indigo-500"
+                    type="text"
+                    placeholder="Add todo..."
+                />
+                <button
+                    onClick={handleAddTodo}
+                    className="border-2 border-indigo-500 bg-indigo-500 text-white p-2 rounded-e-md"
+                >
+                    Add
+                </button>
+            </span>
+            <div></div>
+        </div>
+    );
 }
